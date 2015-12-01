02de2f40784d91430e86f5a81b0002f7abed832b
doojin <dmitry.papka@gmail.com>
2015-12-01 11:08:01 -0200

    Removing _all() method from userRepository

--- spec/repository/user_repository_spec.js
+++ spec/repository/user_repository_spec.js
@@ -2,20 +2,26 @@ var userRepository = require('../../src/repository/user_repository');
 
 describe('userRepository', function() {
 
-    it('exists should return true if user exists', function() {
-        spyOn(userRepository, '_all').and.returnValue([
-            {userID: 'dummyID', password: 'dummyPassword'}
-        ]);
+    var repository;
 
-        expect(userRepository.exists('dummyID', 'dummyPassword')).toBeTruthy();
+    beforeEach(function() {
+        repository = Object.create(userRepository);
+    });
+
+    it('exists should return true if user exists', function() {
+        repository._users = [
+            {userID: 'dummyID', password: 'dummyPassword'}
+        ];
+
+        expect(repository.exists('dummyID', 'dummyPassword')).toBeTruthy();
     });
 
     it('exists should return false if user not exists', function() {
-        spyOn(userRepository, '_all').and.returnValue([
+        repository._users = [
             {userID: 'dummyID', password: 'dummyPassword'}
-        ]);
+        ];
 
-        expect(userRepository.exists('unknown user', 'with unknown password')).toBeFalsy();
+        expect(repository.exists('unknown user', 'with unknown password')).toBeFalsy();
     });
 
 });
--- src/repository/user_repository.js
+++ src/repository/user_repository.js
@@ -4,13 +4,9 @@ userRepository._users = [
     {userID: 'dmitry.papka', password: 'abcdefg123'}
 ];
 
-userRepository._all = function() {
-    return this._users;
-};
-
 userRepository.exists = function(userID, password) {
     var found = false;
-    this._all().forEach(function(user) {
+    this._users.forEach(function(user) {
         if (user.userID === userID && user.password === password) {
             found = true;
         }
